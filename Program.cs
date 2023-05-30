43aa79f27fecccf7ba3608f0b2bf57678fc165d6
Evellyn Caroline <evellynopo@gmail.com>
2023-05-30 15:30:21 -0400

    primeira tentativa

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
-﻿Console.WriteLine("Informe o primeiro valor!");
-int a = int.Parse(Console.ReadLine());
-Console.WriteLine("Informe o segundo valor!");
-int  b = int.Parse(Console.ReadLine());
-int resultado = a / b;
-Console.WriteLine(resultado);
+﻿try
+{
+    Console.WriteLine("Informe o primeiro valor!");
+    int a = int.Parse(Console.ReadLine());
+    Console.WriteLine("Informe o segundo valor!");
+    int b = int.Parse(Console.ReadLine());
+    int resultado = a / b;
+    Console.WriteLine($"O resultado é: "+ resultado);
+
+}catch(Exception ex)
+{
+    Console.WriteLine("Algo deu errado!");
+}
