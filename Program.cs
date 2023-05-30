4eea9aea1049c10103536648823c5be29bd3b08e
Evellyn Caroline <evellynopo@gmail.com>
2023-05-30 15:03:40 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Informe o primeiro valor!");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Informe o segundo valor!");
+int  b = int.Parse(Console.ReadLine());
+int resultado = a / b;
+Console.WriteLine(resultado);
